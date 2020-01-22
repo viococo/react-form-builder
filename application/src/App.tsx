044d023de79cf486e2168efc09e47efda239ac22
viococo <violaine.bodolec@gmail.com>
2020-01-22 21:19:17 +0100

    clean files

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -6,17 +6,60 @@ interface onChangeType {
   value: string;
 }
 
+const inputs = {
+  checkbox: {
+    type: "checkbox",
+    options: {
+      a: { text: "checkbox a" },
+      b: { text: "checkbox b" },
+      c: { text: "checkbox c" }
+    }
+  },
+  radio: {
+    type: "radio",
+    options: {
+      a: { text: "radio a" },
+      b: { text: "radio b" },
+      c: { text: "radio c" }
+    }
+  },
+
+  dropdown: {
+    type: "dropdown",
+    placeholder: "dropdown placehoder",
+    options: {
+      a: { text: "dropdown a" },
+      b: { text: "dropdown b" },
+      c: { text: "dropdown c" }
+    }
+  },
+  select: {
+    type: "select",
+    placeholder: "select placehoder",
+    options: {
+      a: { text: "select a" },
+      b: { text: "select b" },
+      c: { text: "select c" }
+    }
+  },
+
+  number: { type: "number" },
+  plop: { type: "password" },
+  text: { type: "text" },
+  textarea: { type: "textarea", placeholder: "toto" }
+};
+
 const App: React.FC = () => {
-  const [state, setState] = useState({});
+  const [values, setValues] = useState({});
   const onChange = (
     e: React.ChangeEvent<any>,
     { key, value }: onChangeType
   ) => {
-    setState({ ...state, [key]: value });
+    setValues({ ...values, [key]: value });
   };
   useEffect(() => {
-    console.log("STATE", state);
-  }, [state]);
+    console.log("VALUES", values);
+  }, [values]);
 
   const [errors, setErrors] = useState({});
   const onError = ({ key, value }: onChangeType) => {
@@ -29,54 +72,13 @@ const App: React.FC = () => {
   return (
     <FormBuilder
       {...{
-        inputs: {
-          checkbox: {
-            type: "checkbox",
-            options: {
-              a: { text: "checkbox a" },
-              b: { text: "checkbox b" },
-              c: { text: "checkbox c" }
-            }
-          },
-          radio: {
-            type: "radio",
-            options: {
-              a: { text: "radio a" },
-              b: { text: "radio b" },
-              c: { text: "radio c" }
-            }
-          },
-
-          dropdown: {
-            type: "dropdown",
-            placeholder: "dropdown placehoder",
-            options: {
-              a: { text: "dropdown a" },
-              b: { text: "dropdown b" },
-              c: { text: "dropdown c" }
-            }
-          },
-          select: {
-            type: "select",
-            placeholder: "select placehoder",
-            options: {
-              a: { text: "select a" },
-              b: { text: "select b" },
-              c: { text: "select c" }
-            }
-          },
-
-          number: { type: "number" },
-          plop: { type: "password" },
-          text: { type: "text" },
-          textarea: { type: "textarea", placeholder: "toto" }
-        },
+        inputs,
 
         onError,
         onChange,
 
         errors,
-        values: state
+        values
       }}
     />
   );
